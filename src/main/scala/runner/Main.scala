b7fc6e830b2f00bc22cfef4b35fd1c2a88c331f0
Mayur Barge <mayurbarge226@gmail.com>
2020-11-09 16:34:42 +0530

    Revert "CMS-767 Updated the ouput message"
    This reverts commit 02c411a47f3207e32fcdb88aa86dc95adf8d9045.

--- src/main/scala/runner/Main.scala
+++ src/main/scala/runner/Main.scala
@@ -1,26 +1,26 @@
 package runner
 
 import domain._
-import io.{Reader, Writer}
+import io.Reader
+import io.Writer
+import runner.MainApp.validatedSessions
 import scalaz.Scalaz._
-import scalaz.{Validation, _}
+import scalaz._
 import schedular.{ScheduledEvent, Scheduler, SchedulingStrategy}
 import validations.Validator.Result
 
 
 object MainApp extends App {
-  override def main(args: Array[String]): Unit = {
-    val inputFilePath = "./input.txt"
-    val validatedTalks = readTalksFromFile(inputFilePath)
-    val validatedSessions = scheduleAndValidateSessions(validatedTalks)
-    printResults(getSchedule(validatedSessions))
-  }
+  val i = System.getProperty("user.dir")
+  val inputFilePath = "./input.txt"
+  val validatedTalks: Result[List[Talk]] = readTalksFromFile
+  val validatedSessions = scheduleAndValidateSessions
 
-  def readTalksFromFile(inputFilePath: String) = {
+  def readTalksFromFile = {
     Reader.getTokensFromFile(inputFilePath).map(e => Talk.validate(e._1, e._2)).sequence
   }
 
-  def scheduleAndValidateSessions(validatedTalks: Result[List[Talk]]) = {
+  def scheduleAndValidateSessions = {
     for {
       validTalk <- validatedTalks
       validatedSessions = SchedulingStrategy.knapsackScheduling.split(validTalk).map(Session.validatePair).sequence
@@ -29,23 +29,22 @@ object MainApp extends App {
     }
   }
 
-  def getSchedule(validatedSessions: Validation[NonEmptyList[String], Result[List[Tuple2[_ <: MorningSession, _ <: AfternoonSession]]]]) = {
-    validatedSessions.map(_ match {
-      case Success(validSessions) => processSessions(validSessions)
-      case Failure(message) => message.toList.mkString("\n")
-    })
-  }
-
   def processSessions(validSessions: List[(_ <: MorningSession, _ <: AfternoonSession)]): String = {
     val schedules = validSessions.map(e => Track(e._1, e._2)).map(Scheduler.schedule)
     ScheduledEvent.printSchedules(schedules)
   }
 
-  def printResults(result: Validation[NonEmptyList[String], String]) = {
-    result match {
-      case Success(output) => Writer.write(output).unsafePerformIO()
-      case Failure(message) => Writer.write(message.toList.mkString("\n"))
-    }
+  def getSchedule: Validation[NonEmptyList[String], String] = {
+      validatedSessions.map(_ match {
+        case Success(validSessions) => processSessions(validSessions)
+        case Failure(message) => message.toList.mkString("\n")
+      })
+  }
+
+
+  getSchedule match {
+    case Success(output) => Writer.write(output).unsafePerformIO()
+    case Failure(message) => Writer.write(message.toList.mkString("\n"))
   }
 }
 
